ce8c8524e58ad3eb351a19c2a7fb85dea7b20931
Kerollos0Samy0 <kokosamy1111@gmail.com>
2026-09-15 15:26:58 +0300

    Use single hero image for background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,32 +54,14 @@ export default async function Home(props: { searchParams: Promise<{ tab?: string
   let displayedProducts: any[] = [];
   const heroImages = [
     "https://firebasestorage.googleapis.com/v0/b/happyboy01-39e92.firebasestorage.app/o/hero%2Fhappyboy-Ph12862.jpg?alt=media&token=b18eef9f-8423-4425-b888-711bc5efbb05", // Baby Boy
-    "https://firebasestorage.googleapis.com/v0/b/happyboy01-39e92.firebasestorage.app/o/hero%2Fhappyboy-Ph11756.jpg?alt=media&token=c2530200-df9b-4693-8965-67ca6a857ddb", // Middle Boy
-    "https://firebasestorage.googleapis.com/v0/b/happyboy01-39e92.firebasestorage.app/o/hero%2Fhappyboy-Ph11917.jpg?alt=media&token=d8242f5a-6728-4667-a144-3667d08a01c8", // Junior Boy
-    "https://firebasestorage.googleapis.com/v0/b/happyboy01-39e92.firebasestorage.app/o/hero%2Fhappyboy-Ph13081.jpg?alt=media&token=fe83cf80-67c1-4c57-a819-92edf389634a", // Baby Girl
-    "https://firebasestorage.googleapis.com/v0/b/happyboy01-39e92.firebasestorage.app/o/hero%2Fhappyboy-Ph14053.jpg?alt=media&token=e001c900-81f1-4274-881b-e694f0471f87", // Middle Girl
-    "https://firebasestorage.googleapis.com/v0/b/happyboy01-39e92.firebasestorage.app/o/hero%2Fhappyboy-Ph13696.jpg?alt=media&token=9d2f0758-3553-4e1f-812b-c83e99ad79df"  // Junior Girl
-  ];
-  const savedOffsets = [-25, -5, -15, -15, -25, -15];
 
   return (
     <div className="flex flex-col min-h-screen">
       {/* Hero Section */}
       <section className="relative bg-gray-900 text-white overflow-hidden min-h-screen flex items-center justify-center">
         {/* Dynamic Images Background */}
-        <div className="absolute inset-0 z-0 flex">
-          {heroImages.length > 0 ? (
-            heroImages.map((img, i) => (
-              <div key={i} className="flex-1 h-full relative overflow-hidden">
-                {/* Dynamic left offset to align models */}
-                <div className="absolute -inset-x-4 inset-y-0 h-full">
-                  <Image src={img} alt="Hero" fill unoptimized quality={100} priority className="object-cover object-top opacity-95" />
-                </div>
-              </div>
-            ))
-          ) : (
-            <div className="absolute inset-0 bg-gradient-to-r from-[#4B9B9E] to-[#A3292E] opacity-90"></div>
-          )}
+        <div className="absolute inset-0 z-0">
+          <Image src="/hero-all.png" alt="HappyBoy Hero" fill unoptimized quality={100} priority className="object-cover object-top opacity-95" />
         </div>
         
         <div className="relative z-10 max-w-7xl mx-auto text-center py-24 px-4">
