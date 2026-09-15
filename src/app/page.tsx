a2da8cb6755403306400168544d174578e9e15cb
Kerollos0Samy0 <kokosamy1111@gmail.com>
2026-09-15 14:47:30 +0300

    Reorder hero images and set unoptimized for max quality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -53,12 +53,12 @@ export default async function Home(props: { searchParams: Promise<{ tab?: string
 
   let displayedProducts: any[] = [];
   const heroImages = [
-    "https://firebasestorage.googleapis.com/v0/b/happyboy01-39e92.firebasestorage.app/o/hero%2Fhappyboy-Ph13081.jpg?alt=media&token=fe83cf80-67c1-4c57-a819-92edf389634a",
-    "https://firebasestorage.googleapis.com/v0/b/happyboy01-39e92.firebasestorage.app/o/hero%2Fhappyboy-Ph14053.jpg?alt=media&token=e001c900-81f1-4274-881b-e694f0471f87",
-    "https://firebasestorage.googleapis.com/v0/b/happyboy01-39e92.firebasestorage.app/o/hero%2Fhappyboy-Ph13696.jpg?alt=media&token=9d2f0758-3553-4e1f-812b-c83e99ad79df",
-    "https://firebasestorage.googleapis.com/v0/b/happyboy01-39e92.firebasestorage.app/o/hero%2Fhappyboy-Ph12862.jpg?alt=media&token=b18eef9f-8423-4425-b888-711bc5efbb05",
-    "https://firebasestorage.googleapis.com/v0/b/happyboy01-39e92.firebasestorage.app/o/hero%2Fhappyboy-Ph11756.jpg?alt=media&token=c2530200-df9b-4693-8965-67ca6a857ddb",
-    "https://firebasestorage.googleapis.com/v0/b/happyboy01-39e92.firebasestorage.app/o/hero%2Fhappyboy-Ph11917.jpg?alt=media&token=d8242f5a-6728-4667-a144-3667d08a01c8"
+    "https://firebasestorage.googleapis.com/v0/b/happyboy01-39e92.firebasestorage.app/o/hero%2Fhappyboy-Ph11917.jpg?alt=media&token=d8242f5a-6728-4667-a144-3667d08a01c8", // Junior Boy
+    "https://firebasestorage.googleapis.com/v0/b/happyboy01-39e92.firebasestorage.app/o/hero%2Fhappyboy-Ph11756.jpg?alt=media&token=c2530200-df9b-4693-8965-67ca6a857ddb", // Middle Boy
+    "https://firebasestorage.googleapis.com/v0/b/happyboy01-39e92.firebasestorage.app/o/hero%2Fhappyboy-Ph12862.jpg?alt=media&token=b18eef9f-8423-4425-b888-711bc5efbb05", // Baby Boy
+    "https://firebasestorage.googleapis.com/v0/b/happyboy01-39e92.firebasestorage.app/o/hero%2Fhappyboy-Ph13696.jpg?alt=media&token=9d2f0758-3553-4e1f-812b-c83e99ad79df", // Junior Girl
+    "https://firebasestorage.googleapis.com/v0/b/happyboy01-39e92.firebasestorage.app/o/hero%2Fhappyboy-Ph14053.jpg?alt=media&token=e001c900-81f1-4274-881b-e694f0471f87", // Middle Girl
+    "https://firebasestorage.googleapis.com/v0/b/happyboy01-39e92.firebasestorage.app/o/hero%2Fhappyboy-Ph13081.jpg?alt=media&token=fe83cf80-67c1-4c57-a819-92edf389634a"  // Baby Girl
   ];
   const savedOffsets = [-25, -5, -15, -15, -25, -15];
 
@@ -73,7 +73,7 @@ export default async function Home(props: { searchParams: Promise<{ tab?: string
               <div key={i} className="flex-1 h-full relative overflow-hidden">
                 {/* Dynamic left offset to align models */}
                 <div className="absolute -inset-x-4 inset-y-0 h-full">
-                  <Image src={img} alt="Hero" fill sizes="(max-width: 768px) 100vw, 16vw" quality={100} priority className="object-cover object-top opacity-95" />
+                  <Image src={img} alt="Hero" fill unoptimized quality={100} priority className="object-cover object-top opacity-95" />
                 </div>
               </div>
             ))
